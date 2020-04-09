d57dd8422452137e99c8001f917423c49864d85d
Mohammad_Chavoshipor <mohammad.chavoshipor@gmail.com>
2020-04-09 18:50:24 +0430

    update

--- update_per.sh
+++ update_per.sh
@@ -1,2 +1,4 @@
 sudo chown -R www-data '/usr/lib/ckan/default/src/ckan/ckan/public/base/i18n/'
 sudo chmod -R u+rwx '/usr/lib/ckan/default/src/ckan/ckan/public/base/i18n/'
+sudo chown -R www-data /var/lib/ckan/default
+sudo chmod -R u+rwx /var/lib/ckan/default
